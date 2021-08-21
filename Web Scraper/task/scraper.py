26252dc2a4e1d30088f330207cd7297dba6ea7ff
Anna Sosulska <anna.sosulska.stud@pw.edu.pl>
2021-08-21 19:18:53 +0200

    Scrapes for a movie/TV show title and description.

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,20 +1,37 @@
 import requests
+from bs4 import BeautifulSoup
 
 
-def get_response(url, params=None):
+"""This script takes a link to a movie or tv show description and prints scraped title and description to the console.
+It displays "Invalid movie page!" for other websites. Tested for imbd."""
+
+
+def get_response(url: str, params: dict = None) -> requests.Response:
+    """Takes url and optionally parameters (dict) as arguments and returns response object. 
+    English version of the page is enforced."""
     if params is None:
         params = {}
-    return requests.get(url, params=params)
+    return requests.get(url, params=params, headers={"Accept-Language": "en-US,en;q=0.5"})
 
 
-def extract_quote(response):
-    fail_str = "Invalid quote resource!"
+def extract_title(response: requests.Response):
+    """Parses page content for title and description of the film. Returns dictionary object containing title
+     and description. If there is no title and description or there is no response, it returns error message."""
+    fail_str = "Invalid movie page!"
     if response.status_code:
-        return response.json().get('content', fail_str)
+        soup = BeautifulSoup(response.content, "html.parser")
+        title = soup.title.text
+        description = soup.find('meta', {'name': 'description'})["content"]
+        if title and description:
+            return {"title": title, "description": description}
     else:
         return fail_str
 
 
 if __name__ == "__main__":
-    r = get_response(input("Input the URL:\n"))
-    print(extract_quote(r))
+    user_url = input("Input the URL:\n")
+    if "title" in user_url:
+        r = get_response(user_url)
+        print(extract_title(r))
+    else:
+        print("Invalid movie page!")
